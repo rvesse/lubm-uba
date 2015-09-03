862e0b9aca1c02cd4b36a477cf2115e66944b2fc
Rob Vesse <rvesse@dotnetrdf.org>
2015-09-03 16:14:58 +0100

    Some minor improvements
    - generate.sh checks for the JAR file properly
    - generate.sh returns exit code from Java
    - Use fixed JAR name

--- generate.sh
+++ generate.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-java ${JAVA_OPTS} -cp target/uba-1.7.0.jar edu.lehigh.swat.bench.uba.Generator $*
+# Locate ourselves, will fail on BSD/OS X because they don't have readlink -f
+SCRIPT=$(readlink -f $0)
+SCRIPT_DIR=$(dirname "${SCRIPT}")
+
+# Check for the script
+if [ ! -e "${SCRIPT_DIR}/target/lubm-uba.jar" ]; then
+  echo "Failed to find required JAR lubm-uba.jar, pleae ensure you have done a mvn package in ${SCRIPT_DIR} first"
+  exit 1
+fi
+
+# Exec the Java class
+exec java ${JAVA_OPTS} -cp target/lubm-uba.jar edu.lehigh.swat.bench.uba.Generator $*
